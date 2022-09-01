c43cad547b425b8a290d24cefb93aa3c8e17b228
Podlasnisky <jpodlasnisky@gmail.com>
2022-08-31 21:53:11 -0300

    First commit

---
+++ src/test/java/com/github/jpodlasnisky/restwithspringbootjava/StartupTests.java
@@ -0,0 +1,13 @@
+package com.github.jpodlasnisky.restwithspringbootjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
